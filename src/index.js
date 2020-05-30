e0422f5b9d152093e7593873038d836fe9c0ef95
ashwindasr <ashwindasr@gmail.com>
2020-05-30 18:44:42 +0530

    Removed unused comments

--- src/index.js
+++ src/index.js
@@ -16,21 +16,6 @@ function TypeText(props) {
     return (
         mapper
     );
-    // let i = 0;
-    // let firstSplit = props.text.split(" ");
-    // let firstMap = firstSplit.map((item) => (
-    //         item.split("").map((secondItem) =>
-    //                 <div id={"typeText" + i++}>{secondItem}</div>,
-    //
-    //         )
-    //
-    //     // <div>{item}</div>
-    //     )
-
-    // );
-    // return (
-    //     firstMap
-    // );
 }
 
 
@@ -54,32 +39,6 @@ class Main extends React.Component {
 
     checkKey(event) {
         let charCode = event.key.charCodeAt(0);
-
-        //     if (this.state.myText[this.state.myIndex] === event.key) {
-        //         if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode == 32) {
-        //             // console.log("new");
-        //             document.getElementById("typeText" + this.state.myIndex).style.color = "#03fc0b";
-        //             this.setState({
-        //                 myIndex: this.state.myIndex + 1,
-        //             });
-        //         }
-        //
-        // } else if (event.key === "Backspace") {
-        //     console.log("back");
-        //
-        //     this.setState({
-        //         myIndex: this.state.myIndex - 1,
-        //     });
-        //     let temp = this.state.myIndex - 1;
-        //     document.getElementById("typeText" + temp).style.color = "#000000";
-        // } else if (event.key !== "Enter") {
-        //     console.log("enter");
-        //
-        //     document.getElementById("typeText" + this.state.myIndex).style.color = "#fc0303";
-        //     this.setState({
-        //         myIndex: this.state.myIndex + 1,
-        //     });
-        // }
         if (event.key === "Enter") {
             let minutes = 1000 * 60;
             let d = new Date();
@@ -99,11 +58,9 @@ class Main extends React.Component {
 
             document.getElementById("speed").innerHTML = "Your speed: " + Math.round(this.state.myText.split(" ").length / this.totalTime) + " WPM";
             document.getElementById("accuracy").innerHTML = "Your accuracy: " + Math.round(((this.state.letters - this.state.incorrect) / this.state.letters) * 100)
-        }
-        else if (event.key === "CapsLock" || event.key === "Shift"){
+        } else if (event.key === "CapsLock" || event.key === "Shift") {
             console.log("caps");
-        }
-    else if (event.key === "Backspace"){
+        } else if (event.key === "Backspace") {
             if (this.state.myIndex == 0) {
                 return;
             }
@@ -113,10 +70,8 @@ class Main extends React.Component {
             });
             let temp = this.state.myIndex - 1;
             document.getElementById("typeText" + temp).style.color = "#000000";
-        }
-
-        else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode == 46) {
-            if (this.state.myIndex > this.state.totalIndex){
+        } else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode == 46) {
+            if (this.state.myIndex > this.state.totalIndex) {
                 return;
             }
             if (this.state.myText[this.state.myIndex] === event.key) {
@@ -124,19 +79,16 @@ class Main extends React.Component {
                 this.setState({
                     myIndex: this.state.myIndex + 1,
                 });
-            }
-            else {
-                    // console.log("enter");
-
-                    document.getElementById("typeText" + this.state.myIndex).style.color = "#fc0303";
-                    this.setState({
-                        myIndex: this.state.myIndex + 1,
-                        incorrect: this.state.incorrect + 1,
-                    });
+            } else {
+                document.getElementById("typeText" + this.state.myIndex).style.color = "#fc0303";
+                this.setState({
+                    myIndex: this.state.myIndex + 1,
+                    incorrect: this.state.incorrect + 1,
+                });
             }
 
         } else if (charCode === 32) {
-            if (this.state.myIndex > this.state.totalIndex){
+            if (this.state.myIndex > this.state.totalIndex) {
                 return;
             }
             this.setState({
@@ -156,24 +108,6 @@ class Main extends React.Component {
                 startTime: n,
             });
         }
-
-        // if (event.key === "Enter") {
-        //     let minutes = 1000 * 60;
-        //     let d = new Date();
-        //     let n = d.getTime();
-        //
-        //     // Calculate total time
-        //     this.totalTime = (n - this.state.startTime) / minutes;
-        //     console.log("Total time: " + this.totalTime);
-        //
-        //     let value = event.target.value;
-        //     if (value.replace("\n", "") === this.state.myText) {
-        //         console.log("Correct");
-        //     } else {
-        //         console.log("Incorrect");
-        //     }
-        //     event.target.value = "";
-        // }
     }
 
     render() {
@@ -182,8 +116,12 @@ class Main extends React.Component {
                 <Row><h1 className="align-content-center">Speed Test</h1></Row>
                 <Row className="myRow"><TypeText text={this.state.myText}/></Row>
                 <Row><textarea id="textInput" onKeyDown={this.checkKey}/></Row>
-                <Row className="speed-div"><div id="speed"></div></Row>
-                <Row className="accuracy-div"><div id="accuracy"></div></Row>
+                <Row className="speed-div">
+                    <div id="speed"></div>
+                </Row>
+                <Row className="accuracy-div">
+                    <div id="accuracy"></div>
+                </Row>
             </Container>
         );
     }
