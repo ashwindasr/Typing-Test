d0ede34a42bb65781d5a1ad599fd61b96be18c60
ashwindasr <ashwindasr@gmail.com>
2020-05-30 15:53:40 +0530

    Working code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "reactstrap/es/Container";
+import Row from "reactstrap/es/Row";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function TypeText(props) {
+    let text = props.text.split("");
+    let i = 0;
+    let mapper = text.map((item) =>
+            <div id={"typeText" + i++}>{item}</div>
+    );
+    return (
+        mapper
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+class Main extends React.Component{
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                myText: "apple is red",
+                myIndex: 0,
+                startTime: null,
+                words: 3,
+                totalTime: null,        // in minutes
+            };
+            this.checkKey = this.checkKey.bind(this)
+        }
+
+
+
+    checkKey(event){
+
+
+
+
+        if (this.state.myText[this.state.myIndex] === event.key){
+            // console.log("new");
+            document.getElementById("typeText" + this.state.myIndex).style.color = "#03fc0b";
+            this.setState({
+                myIndex: this.state.myIndex + 1,
+            });
+        } else if (event.key === "Backspace"){
+            // console.log("back");
+
+            this.setState({
+                myIndex: this.state.myIndex - 1,
+            });
+            let temp = this.state.myIndex -1 ;
+            document.getElementById("typeText" + temp).style.color = "#000000";
+        }
+        else if (event.key !== "Enter") {
+            // console.log("enter");
+
+            document.getElementById("typeText" + this.state.myIndex).style.color = "#fc0303";
+            this.setState({
+                myIndex: this.state.myIndex + 1,
+            });
+        }
+
+        console.log(this.state.myIndex);
+
+        if (event.key === "a"){
+            let d = new Date();
+            let n = d.getTime();
+
+            // Get start time
+            // this.state.startTime = n;
+            this.setState({
+                startTime: n,
+            });
+        }
+
+        if (event.key === "Enter"){
+            let minutes = 1000 * 60;
+            let d = new Date();
+            let n = d.getTime();
+
+            // Calculate total time
+            this.totalTime = (n - this.state.startTime) / minutes;
+            console.log("Total time: " + this.totalTime);
+
+            let value = event.target.value;
+            if (value.replace("\n", "") === this.state.myText) {
+                console.log("Correct");
+            } else {
+                console.log("Incorrect");
+            }
+            event.target.value = "";
+        }
+    }
+    render() {
+        return (
+            <Container>
+                <Row><TypeText text={this.state.myText} /></Row>
+                <Row><textarea id="textInput" onKeyDown={this.checkKey}/></Row>
+            </Container>
+        );
+    }
+
+
+}
+
+
+ReactDOM.render(<Main/>, document.getElementById("root"));
